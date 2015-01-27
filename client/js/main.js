561491f5d4f2ec38d72d9caafa5665efa1918159
Zoe Ames <zoeames@gmail.com>
2015-01-27 02:27:21 -0600

    button colors changed, calculations updated

--- client/js/main.js
+++ client/js/main.js
@@ -14,22 +14,26 @@
 
     $('#createRule').click(function(){
       var amount = $('#ruleSign option:selected').text() + $('#rule').val(),
-          htmlString = '<tr><td>'+ $('#ruleText').val()+'</td><td>'+ $('#ruleSign option:selected').text() +'</td><td>'+ $('#rule').val() + '</td><td> <button class="applyButton" type="submit" value='+ amount +'>Apply</button></td></tr>';
+          htmlString = '<tr><td>'+ $('#ruleText').val()+'</td><td>'+ $('#ruleSign option:selected').text() +'</td><td>'+ $('#rule').val() + '</td><td> <button class="applyButton btn yellow" type="submit" value='+ amount +'>Apply</button></td></tr>';
       //console.log(ammount);
       $('#rulesTable tr:last').after(htmlString);
-      /*
-      if($('#ruleSign option:selected').text() === '\+'){
-        rate.increaseRate($('#rule').val());
-      }
-      if($('#ruleSign option:selected').text() === '\-'){
-        rate.decreaseRate($('#rule').val());
-      }*/
     });
 
     $('#rulesTable').on('click', '.applyButton', function(){
-      alert('buton clicked!');
-      //var amount = parseInt($(this).val());
-      console.log(this);
+      var amount = parseInt($(this).val());
+      console.log(amount);
+      if(amount<0){
+        rate.decreaseRate(amount*-1);              //removes negative sign
+        console.log('negative Number');
+        $(this).parent().remove();                //removes button so can't apply rule more than once
+        $('#finalHourlyRate').html(rate.finalAmount);
+
+      }else{
+        rate.increaseRate(amount);
+        console.log('positive number');
+        $(this).parent().remove();             //removes button so can't apply rule more than once
+        $('#finalHourlyRate').html(rate.finalAmount);
+      }
       //return amount;
     });
 
--- client/js/rate.js
+++ client/js/rate.js
@@ -7,10 +7,10 @@
   }
 
   Rate.prototype.finalRate = function(){
-    var finalRate = this.baseRate;
-    this.finalAmount += finalRate;
-    console.log('this.finalamount', typeof this.finalAmount, this.finalAmount);
-    return finalRate;
+    //var finalRate = this.baseRate;
+    this.finalAmount += this.baseRate;
+    //console.log('this.finalamount', typeof this.finalAmount, this.finalAmount);
+    //return finalAmount;
   };
 
   Rate.prototype.updateBase = function(num){
